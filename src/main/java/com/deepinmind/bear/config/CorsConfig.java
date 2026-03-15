863955ece29bee9f9429393edff13ea03a7f40d6
spidercoco1986 <2578902032@qq.com>
2026-03-15 22:20:39 +0800

    auto commit aibeary

---
+++ src/main/java/com/deepinmind/bear/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.deepinmind.bear.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://it.deepinmind.com", "https://it.deepinmind.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
